5e0aca12b5ecd5a518ac1fd74043189f469147bd
Brian Mullin <brmullin@email.msmary.edu>
2017-07-08 21:33:00 -0400

    Added repository interfaces amended author

---
+++ src/main/java/quickpoll/repository/OptionRepository.java
@@ -0,0 +1,12 @@
+package quickpoll.repository;
+
+import com.quickpoll.Option;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by brianmullin on 7/8/17.
+ */
+public interface OptionRepository extends CrudRepository<Option, Long>{
+
+
+}
